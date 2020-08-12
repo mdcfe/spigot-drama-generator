97afd6c9e77afc3825a0814ff1d9e4712c20539e
md678685 <1917406+md678685@users.noreply.github.com>
2020-08-12 21:10:34 +0100

    Add Twitter link
    Courtesy of @Chew

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function renderDrama(message, share, sharePath, teaser) {
         <title>Spigot Drama Generator</title>
         <meta name="description" content="${message}" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <meta name="og:title" content="${teaser}"/>
         <meta name="og:type" content="website"/>
         <meta name="og:url" content="${share}"/>
@@ -67,7 +67,19 @@ function renderDrama(message, share, sharePath, teaser) {
         <h3>Spigot Drama Generator</h3>
         <h1>${message}</h1>
         <span id="fight"></span>
-        <h6><a id="more" href="/">Generate more drama!</a> (or press enter)<br /><br />This website is made in jest - don't take it too seriously!<br />Developed by md678685; PRs welcome on <a href="https://github.com/md678685/spigot-drama-generator">GitHub</a>.<br />Inspired by (and heavily borrows from) <a href="https://github.com/asiekierka/MinecraftDramaGenerator/">asiekierka's Minecraft Drama Generator</a>.<br /><br /><a href="https://www.reddit.com/r/mbax/comments/hwhfua/a_letter_to_the_redditor/">RIP r/admincraft</a></h6>
+        <h6>
+            <a id="more" href="/">Generate more drama!</a> (or press enter)
+            <br /><br />
+            This website is made in jest - don't take it too seriously!
+            <br />
+            Developed by md678685; PRs welcome on <a href="https://github.com/md678685/spigot-drama-generator">GitHub</a>.
+            <br />
+            <i>Now available in <a href="https://twitter.com/SpigotDrama">Twitter</a> form!</i>
+            <br />
+            Inspired by (and heavily borrows from) <a href="https://github.com/asiekierka/MinecraftDramaGenerator/">asiekierka's Minecraft Drama Generator</a>.
+            <br /><br />
+            <a href="https://www.reddit.com/r/mbax/comments/hwhfua/a_letter_to_the_redditor/">RIP r/admincraft</a>
+        </h6>
     </body>
 </html>
     `
@@ -81,7 +93,7 @@ function handleRoot(url) {
     for (key in combinations) {
         drama[key] = [randomIndex(combinations[key]), randomIndex(combinations[key]), randomIndex(combinations[key]), randomIndex(combinations[key])];
     }
-    
+
     const dramaUrl = btoa(JSON.stringify(drama));
     const host = url.host == "example.com" ? "localhost:8787" : url.host;
 
@@ -106,7 +118,7 @@ function handleDrama(url) {
                 message = message.replace(placeholder, replacement);
             }
         }
-        
+
         url.pathname = "/" + btoa(JSON.stringify(usedDramaIds));
 
         let teaser = randomEntry(social);
